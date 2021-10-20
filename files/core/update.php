a9646bb550c8af96984e58f517b714bd18467d2c
Jarnick Puaw <92668124+lucyfer622@users.noreply.github.com>
2021-10-20 18:06:27 +0100

    Update update.php
    edited for new and updated link and system

--- files/core/update.php
+++ files/core/update.php
@@ -5,7 +5,7 @@ $installedVersion = trim(file_get_contents('includes/version'));
 $currentVersion = trim(file_get_contents('https://raw.githubusercontent.com/lucyfer622/whmseller/main/version'));
 
 if ($installedVersion < $currentVersion) {
-	$output = shell_exec('rm whmseller.zip -f; wget https://www.whmseller.com/update/v2/whmseller.zip; unzip -o whmseller.zip -d /usr/local/cpanel/whostmgr/docroot/cgi/whmseller; rm whmseller.zip -f; dos2unix /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/sellercp/index.cgi; chmod +x /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/sellercp/index.cgi');
+	$output = shell_exec('rm /root/whmseller.zip -f; wget -O /root/whmseller.zip https://github.com/lucyfer622/whmseller/archive/refs/heads/main.zip; unzip -o /root/whmseller.zip -d /root/; rm whmseller.zip -f; cp -Rf ./whmseller-main/files/core/* /usr/local/cpanel/whostmgr/docroot/cgi/whmseller; unzip /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/assets/vendor.zip -d /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/assets/; rm -rf /root/whmseller-main; dos2unix /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/sellercp/index.cgi; chmod +x /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/sellercp/index.cgi');
 	include 'afterupdate.php';
 }
 
