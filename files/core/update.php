055839fb6d8e67fa0bfb9298a985916ae76cc360
Jarnick Puaw <92668124+lucyfer622@users.noreply.github.com>
2021-10-21 18:15:00 +0100

    Update update.php

--- files/core/update.php
+++ files/core/update.php
@@ -2,8 +2,8 @@
 
 
 $installedVersion = trim(file_get_contents('includes/version'));
-$currentVersion = trim(file_get_contents('https://raw.githubusercontent.com/lucyfer622/whmseller/main/version'));
-
+//$currentVersion = trim(file_get_contents('https://raw.githubusercontent.com/lucyfer622/whmseller/main/version'));
+$currentVersion = shell_exec('curl -s https://raw.githubusercontent.com/lucyfer622/whmseller/main/version');
 if ($installedVersion < $currentVersion) {
 	$output = shell_exec('rm /root/whmseller.zip -f; wget -O /root/whmseller.zip https://github.com/lucyfer622/whmseller/archive/refs/heads/main.zip; unzip -o /root/whmseller.zip -d /root/; rm /root/whmseller.zip -f; mv -bfv /root/whmseller-main/files/core/* /usr/local/cpanel/whostmgr/docroot/cgi/whmseller; unzip /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/assets/vendor.zip -d /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/assets/; rm -rf /root/whmseller-main; rm -rf /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/assets/vendor.zip; dos2unix /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/sellercp/index.cgi; chmod +x /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/sellercp/index.cgi');
 	include 'afterupdate.php';
