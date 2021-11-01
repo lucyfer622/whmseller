d6733f4938f49ac2424a679d354d9b15dc17881c
Jarnick Puaw <92668124+lucyfer622@users.noreply.github.com>
2021-11-01 16:11:48 +0000

    Create uninstall.sh
    Requested on Discord for a uninstall script ;x

---
+++ uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+IP=$(hostname -i);
+HOST=$(hostname);
+echo "#####################################################################################";
+echo "#####################################################################################";
+echo "#####                                                                           #####";
+echo "#####                            WHMSELLER Uninstall                            #####";
+echo "#####                                                                           #####";
+echo "##### Written and maintained by https://github.com/lucyfer622/whmseller/        #####";
+echo "##### Email lucifer_622@hotmail.com for any questions regarding this module     #####";
+echo "##### We are kind sad to see you leave, but please help the plugin grow.        #####";
+echo "#####################################################################################";
+echo "#####################################################################################";
+echo "";
+echo "#####################################################################################";
+echo "|Website : https://github.com/lucyfer622/whmseller/";
+echo "|Server Ip : $IP";
+echo "|Hostname : $HOST";
+echo "";
+echo "Whmseller uninstalling....";
+/usr/local/cpanel/bin/unregister_appconfig /usr/local/cpanel/whostmgr/docroot/cgi/whmseller/whmseller.conf;
+rm -rf /usr/local/cpanel/whostmgr/docroot/cgi/addon_whmseller.cgi;
+rm -rf /usr/local/cpanel/whostmgr/docroot/cgi/whmseller;
+/usr/bin/crontab -l | grep -v 'whmseller' >> /var/spool/cron/root;
+/bin/systemctl restart cpanel.service > /dev/null 2>&1;
+echo "Hope we see you in near future ;) You can allways count on our plugin to your needs";
+rm -rf uninstall.sh;
