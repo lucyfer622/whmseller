0c0fcbcc25ad1bc44bdfba983838e95c5d519aad
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-11-08 06:38:35 +0000

    Update integration.php

--- files/core/integration.php
+++ files/core/integration.php
@@ -19,7 +19,7 @@ echo '<tbody><tr><td><a href="#"><img src="assets/img/brand/whmcs.png" height="3
  
 echo "\r\n" . ' </td>'."\r\n".'</tbody>'."\r\n".'</tr></table>'."\r\n".'</div>'."\r\n".'</div>'."\r\n".'</div>'."\r\n".'<div class="col-xl-4 mt-0"><div class="card shadow">'."\r\n".'<div class="card-header border-0">'."\r\n".'<div class="row align-items-center">'."\r\n".'<div class="col">'."\r\n".'<h3 class="mb-0">Installation Guide</h3>'."\r\n".'</div>'."\r\n".'</div>'."\r\n".'</div>
 <div class="card card-stats">
-<div class="card-body"><b>1.</b>You need to be download whmcsmoudle.zip<br><b>2.</b>Upload zip file on whmcs root directory and extract.<br><b>3.</b> Create Server from WHMCS Admin Panel. Under Server details, choose WHMSELLER.<br><b>4.</b> Under Setup > Products/Services, Create a Reseller Package. In Module Settings Tab, Choose WHMSELLER as Module Name.<br><b>5.</b> Complete the form and choose the option for Account to Create.<br>If you need anykind of help, Open a support ticket<b><a href="https://www.hostqweb.com"> #Needsupport</b></a>
+<div class="card-body"><b>1.</b>You need to be download whmcsmoudle.zip<br><b>2.</b>Upload zip file on whmcs root directory and extract.<br><b>3.</b> Create Server from WHMCS Admin Panel. Under Server details, choose WHMSELLER.<br><b>4.</b> Under Setup > Products/Services, Create a Reseller Package. In Module Settings Tab, Choose WHMSELLER as Module Name.<br><b>5.</b> Complete the form and choose the option for Account to Create.<br>If you need anykind of help, Open a support ticket<b><a href="https://github.com/lucyfer622/whmseller/issues"> #Needsupport</b></a>
 </div>
  
 </div></div></div>';
